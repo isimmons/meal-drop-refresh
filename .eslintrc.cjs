f08a6dd4284df0589bc66ccc5b80eedbea00864c
isimmons <isimmons33@gmail.com>
2023-06-16 21:25:07 -0400

    fix esling/prettier/airnbn config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,16 +1,54 @@
 module.exports = {
   env: {
     browser: true,
-    es2020: true
+    es2021: true,
   },
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:react-hooks/recommended", "plugin:storybook/recommended"],
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'airbnb-typescript',
+    'plugin:prettier/recommended',
+    'plugin:storybook/recommended',
+  ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
     ecmaVersion: 'latest',
-    sourceType: 'module'
+    sourceType: 'module',
+    project: 'tsconfig.eslint.json',
   },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', '@typescript-eslint', 'import'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
   rules: {
-    'react-refresh/only-export-components': 'warn'
-  }
-};
+    'import/no-extraneous-dependencies': 'off',
+    'import/no-anonymous-default-export': 'off',
+    'prettier/prettier': 'warn',
+    'import/order': ['warn', { 'newlines-between': 'always' }],
+    '@typescript-eslint/no-use-before-define': 'off',
+    'react-refresh/only-export-components': 'warn',
+    'react/jsx-uses-react': 'off',
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'import/prefer-default-export': 'off',
+    'import/extensions': 'off',
+    'react/require-default-props': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'no-nested-ternary': 'off',
+    'no-console': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    'react/no-unused-prop-types': 'off',
+    'no-param-reassign': 'off',
+    'class-methods-use-this': 'off',
+    'prefer-promise-reject-errors': 'off',
+    'react/prefer-stateless-function': 'off',
+    '@typescript-eslint/no-implied-eval': 'off',
+    'import/no-cycle': 'off',
+    'react/no-array-index-key': 'off',
+  },
+}
--- src/pages/RestaurantDetailPage/RestaurantDetailPage.stories.tsx
+++ src/pages/RestaurantDetailPage/RestaurantDetailPage.stories.tsx
@@ -1,6 +1,6 @@
-import type { Decorator, Meta, StoryObj } from "@storybook/react";
+import type { Decorator, Meta, StoryObj } from '@storybook/react'
 
-import { RestaurantDetailPage } from "./RestaurantDetailPage";
+import { RestaurantDetailPage } from './RestaurantDetailPage'
 
 const insertPortalDiv: Decorator = (Story) => {
   return (
@@ -8,31 +8,31 @@ const insertPortalDiv: Decorator = (Story) => {
       <Story />
       <div id="modal"></div>
     </>
-  );
-};
+  )
+}
 
 const meta: Meta<typeof RestaurantDetailPage> = {
-  title: "Pages/RestaurantDetailPage",
+  title: 'Pages/RestaurantDetailPage',
   component: RestaurantDetailPage,
   decorators: [insertPortalDiv],
   parameters: {
-    layout: "fullscreen",
+    layout: 'fullscreen',
     deepLink: {
-      path: "/restaurants/:id",
-      route: "/restaurants/1",
+      path: '/restaurants/:id',
+      route: '/restaurants/1',
     },
   },
-};
+}
 
-export default meta;
+export default meta
 
-type Story = StoryObj<typeof meta>;
+type Story = StoryObj<typeof meta>
 
 export const Success: Story = {
   parameters: {
     design: {
-      type: "figma",
-      url: "https://www.figma.com/proto/3Q1HTCalD0lJnNvcMoEw1x/Mealdrop?page-id=135%3A257&type=design&node-id=169-510&viewport=-1250%2C1021%2C0.5&scaling=min-zoom&starting-point-node-id=135%3A258",
+      type: 'figma',
+      url: 'https://www.figma.com/proto/3Q1HTCalD0lJnNvcMoEw1x/Mealdrop?page-id=135%3A257&type=design&node-id=169-510&viewport=-1250%2C1021%2C0.5&scaling=min-zoom&starting-point-node-id=135%3A258',
     },
   },
-};
+}
